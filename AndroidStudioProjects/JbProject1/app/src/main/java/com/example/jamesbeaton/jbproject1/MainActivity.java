78dbb402925f7fe650e151a18a2abad4f5a67972
jamesbeaton <skillumination@gmail.com>
2017-08-07 04:26:37 +0100

    JB Project 1

--- AndroidStudioProjects/JbProject1/app/src/main/java/com/example/jamesbeaton/jbproject1/MainActivity.java
+++ AndroidStudioProjects/JbProject1/app/src/main/java/com/example/jamesbeaton/jbproject1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jamesbeaton.jbproject1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
